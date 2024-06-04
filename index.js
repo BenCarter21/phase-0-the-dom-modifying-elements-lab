45974f4c6912dffe3af65d15559d93a17dbb6864
Ben <bc@bencarterconsulting.com>
2024-06-04 13:50:18 -0400

    Assignment adjustments made.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = 'Ben Carter is the champion'
+// Create a new paragraph element
+//const newParagraph = document.createElement('p');
+
+// Set the text content for the paragraph
+//newParagraph.textContent = 'Ben Carter is the champion';
+// Append the paragraph to an existing element in the document
+//document.body.appendChild(newParagraph);
